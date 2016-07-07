e11716a9de35e20434864f71fdc9b76b20d46bcc
JessMitch <jessicamitch42@gmail.com>
2016-07-07 18:55:43 +0000

    exericse 2!

--- answers.js
+++ answers.js
@@ -1,13 +1,23 @@
 //Exercise 1
 
-var numbersList = [-5, -4, 5, 4, -6, -100, 66];
+var numbersList = [-5, -4, 5, 4, -6, -100, 66, 7, 8888];
 var positiveNum = [];
 
 
-numbersList.forEach(function(num) {
-    if (num > 0) {
-        positiveNum.push(num);
-    }
-});
+// numbersList.forEach(function(num) {
+//     if (num > 0) {
+//         positiveNum.push(num);
+//     }
+// });
 
-console.log(positiveNum);
+// console.log(positiveNum);
+
+//Exercise 2!
+
+
+function getPositive(num) {
+    if (num > 0) {
+        return num;
+    }
+}
+console.log(numbersList.filter(getPositive));
